6e2e399beb625de5bfc356d0a24fc8bdfe6a39c9
badhashsolution <badhashsolution@gmail.com>
2013-12-29 08:58:27 -0800

    admin working as of 42:47

--- microblog/settings/base.py
+++ microblog/settings/base.py
@@ -65,7 +65,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ("..", "static")
+STATIC_ROOT = root("..", "static")
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -78,7 +78,7 @@ STATICFILES_DIRS = (
     # Don't forget to use absolute paths, not relative paths.
 
     # WGG note that we've changed this with our lambda script at the top
-    root("assets")
+    root("assets"),
 )
 
 # List of finder classes that know how to find static files in
@@ -118,7 +118,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    root("templates")
+    root("templates"),
 )
 
 DJANGO_APPS = (
@@ -129,7 +129,7 @@ DJANGO_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    #'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
