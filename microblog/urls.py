6567d686c424ed7c46b8b3f16e1041cd744f806e
badhashsolution <badhashsolution@gmail.com>
2013-12-29 18:06:04 +0000

    added hello world template

--- microblog/urls.py
+++ microblog/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 admin.autodiscover()
 
@@ -15,4 +16,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', TemplateView.as_view(template_name="index.html")),
 )
