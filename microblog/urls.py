337b87a0fc3cc1ef3e6eebd416d1ed7a3021f1c4
badhashsolution <badhashsolution@gmail.com>
2013-12-30 14:12:59 +0000

    working as of 29:51

--- microblog/urls.py
+++ microblog/urls.py
@@ -1,27 +1,12 @@
 from django.conf.urls import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
-
-from blog.views import blog_list, blog_detail
 from . import views
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'microblog.views.home', name='home'),
-    # url(r'^microblog/', include('microblog.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     url(r'^$', views.HomepageView.as_view(), name="home"),
-    url(r'^blog/$', blog_list, name="blog_list"),
-    url(r"^blog/(?P<pk>\d+)/$", blog_detail, name = "blog_detail"),
-
-
+    url(r'^blog/', include('blog.urls', namespace=views)),
     url(r'^admin/', include(admin.site.urls)),
 
 )
