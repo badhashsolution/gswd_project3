d74ea1ead56582818eb35ab0f78be759d10d05b4
badhashsolution <badhashsolution@gmail.com>
2013-12-30 12:52:28 +0000

    working as of 22:51 of video 2

--- blog/views.py
+++ blog/views.py
@@ -10,3 +10,13 @@ def blog_list(request, *args, **kwargs):
         "post_list": post_list
     }
     return render(request, template_name, context)
+
+def blog_detail(request, pk, *args, **kwargs):
+    post = Post.objects.get(pk= pk, published = True)
+    template_name = "post_detail.html"
+
+    context = {
+        "post": post
+    }
+
+    return request(request, template_name, context)
