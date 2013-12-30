cbcc98c7a90dfa5fb81759f8d0713106b7665bd8
badhashsolution <badhashsolution@gmail.com>
2013-12-30 18:13:13 +0000

    working 34:10

--- blog/views.py
+++ blog/views.py
@@ -3,33 +3,13 @@ from django.views.generic import ListView, DetailView
 
 from .models import Post
 
-def blog_list(request, *args, **kwargs):
-    post_list = Post.objects.filter(published=True)
-    template_name = "post_list.html"
-
-    context = {
-        "post_list": post_list
-    }
-    return render(request, template_name, context)
-
-class PostListView(ListView):
-    model = Post
-
-    # WGG over-ride the query set on the class to get only published posts
+class PublishPostsMixin(object):
     def get_queryset(self):
-        queryset = super(PostListView, self).get_queryset()
+        queryset = super(PublishPostsMixin, self).get_queryset()
         return queryset.filter(published=True)
 
-def blog_detail(request, pk, *args, **kwargs):
-    post = Post.objects.get(pk= pk, published=True)
-    template_name = "blog/post_detail.html"
-
-    context = {
-        "post": post
-    }
-
-class PostDetailView(DetailView):
+class PostListView(PublishPostsMixin, ListView):
     model = Post
-    def get_queryset(self):
-        queryset = super(PostDetailView, self).get_queryset()
-        return queryset.filter(published=True)
+
+class PostDetailView(PublishPostsMixin, DetailView):
+    model = Post
